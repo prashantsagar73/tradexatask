8d4f91266589cc884a417bb50274dcce9c84fbbc
prashantsagar73 <prashant.sagar73@gmail.com>
2021-08-17 21:44:49 +0530

    update username and profile pic

--- user/models.py
+++ user/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
+
 class Post(models.Model):
     title = models.TextField(max_length=150)
     content = models.CharField(max_length=5000)
@@ -11,3 +13,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg',height_field=None, width_field=None, upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    # resize the image of user
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
