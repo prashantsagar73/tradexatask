a84fc18972c2f02abdb5c037b5f87de5af1a414e
prashantsagar73 <prashant.sagar73@gmail.com>
2021-08-17 18:13:13 +0530

    login logout and porfile of user

--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -20,8 +21,12 @@ def register (request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('Blog_home')
+            messages.success(request, f' Your account has been created! Now you are able to login.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request,'profile.html')
